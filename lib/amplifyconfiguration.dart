5cae46516ce3871a531d5928adc0bf16324255e7
Mohamad-Haniff <haniffmpk2021@gmail.com>
2020-12-29 23:48:42 +0800

    AWS

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:3c791d66-1964-4aee-aa20-b9ac038ef666",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_4Vd9U6uff",
+                        "AppClientId": "73t1rq7voljejd8vscqhoiqtg0",
+                        "AppClientSecret": "13uoen6sf49reps41lvh6ose1ra4sp7jbrvilkqurbtd5q8vhkdi",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
